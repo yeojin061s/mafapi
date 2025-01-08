919332f49fa3216002e06a9a117d397ff6fab245
yeojin061s <mintyeobseo@gmail.com>
2025-01-08 21:12:52 +0900

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, request, jsonify
 import requests
 import json
 import sqlite3
+import os
 
 
 app = Flask(__name__)
@@ -309,5 +310,8 @@ def update_user():
 
 
 # 메인 실행
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    port = int(os.environ.get("PORT", 5000))  # 기본값은 5000
+    app.run(host="0.0.0.0", port=port)
+
